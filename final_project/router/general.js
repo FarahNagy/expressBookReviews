23f6d9bae35cdbe8bc78cd86eb507df1564c0a53
FarahNagy <farahalaa0529@outlook.com>
2026-03-09 00:14:21 +0200

    task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,7 +33,19 @@ public_users.get('/isbn/:isbn', function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let result = [];
+  for (let isbn in books) {
+    if (books[isbn].author === req.params.author) {
+      result.push(books[isbn]);
+    }
+  }
+
+  if(result.length>0){
+    res.send(JSON.stringify(result));
+  }
+  else {
+    return res.status(403).json({message:"no matches."});
+  }
 });
 
 // Get all books based on title
