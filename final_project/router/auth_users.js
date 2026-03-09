23c1817311131e0ea39e359d1d63cad9aaaa298e
FarahNagy <farahalaa0529@outlook.com>
2026-03-09 02:59:48 +0200

    task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -8,23 +8,42 @@ let users = [];
 const isValid = (username) => { //returns boolean
   //write code to check is the username is valid
   for (let id in users) {
-    console.log("printing user ",id)
+    console.log("printing user ", id)
     console.log("users count!: ", users.length)
     if (users[id].username === username)
       return false;
   }
-    return true;
+  return true;
 
 }
 
 const authenticatedUser = (username, password) => { //returns boolean
   //write code to check if username and password match the one we have in records.
+  for (i in users) {
+    if (users[i].username === username && users[i].password === password) {
+      return true;
+    }
+  }
+    return false;
 
 }
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
   //Write your code here
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (username && password) {
+    if (authenticatedUser(username, password)) {
+      let accessToken = jwt.sign({ data: password }, 'access', { expiresIn: 60 * 60 });
+      req.session.authorization = {
+        accessToken, username
+      }
+      return res.status(200).send(`${username} successfully logged in`);
+    }
+  }
+
   return res.status(300).json({ message: "Yet to be implemented" });
 });
 
